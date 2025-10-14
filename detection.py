d865d207c513b5f0614b582690f4407ab4df24a1
sandip <sandipkhamkar564>
2025-10-14 23:51:47 +0530

    CrowdTrackAI Streamlit App

--- detection.py
+++ detection.py
@@ -1,14 +1,14 @@
-# detection.py
 from ultralytics import YOLO
-from config import YOLO_MODEL
 
-# Load YOLOv8 model
-model = YOLO(YOLO_MODEL)
+# Load local YOLOv8 model
+model = YOLO("yolov8n.pt")  # make sure this file exists in your project folder
 
 def detect_people(frame):
     results = model(frame)
     detections = []
-    for r in results[0].boxes.xyxy:
-        x1, y1, x2, y2 = map(int, r)
-        detections.append([x1, y1, x2-x1, y2-y1, 1.0])
+    for box, cls in zip(results[0].boxes.xyxy, results[0].boxes.cls):
+        if int(cls) == 0:  # class 0 = person
+            x1, y1, x2, y2 = map(int, box)
+            w, h = x2 - x1, y2 - y1
+            detections.append([x1, y1, w, h, 1.0])
     return detections
