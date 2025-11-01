4fbd9f1724ee3f904f24f8a3fd495d60da4146a4
sandip <sandipkhamkar564>
2025-11-01 10:43:39 +0530

    Updated project files

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 YOLO_MODEL = "yolov8n.pt"    # Small model for speed
-MAX_PEOPLE = 10              # Overcrowding threshold
+MAX_PEOPLE = 1000              # Overcrowding threshold
 VIDEO_OUTPUT = "output.mp4"  # Default output video name
